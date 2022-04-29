1bd812e1ad09b45765e7d5bff613f019e2c3cf92
AoWangPhilly <aw3338@drexel.edu>
2022-04-28 22:50:18 -0400

    Test out github actions

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,9 +1,11 @@
 from .example import example
 
+
 def capitalize_string(s):
     if not isinstance(s, str):
         raise TypeError('Please provide a string')
     return s.capitalize()
+
 
 def test_capitalize_string():
     assert example.capitalize_string('test') == 'Test'
