7d466c0e92779485ba90e10d5ae696c3da1532e7
AoWangPhilly <aw3338@drexel.edu>
2022-05-04 12:45:42 -0400

    Add in InvalidTailError and docstrings

--- src/ConfidenceInterval/Error.py
+++ src/ConfidenceInterval/Error.py
@@ -1,2 +1,6 @@
 class InvalidScoreError(Exception):
-    ...
+    """The critical value must either be a Z or T distribution"""
+
+
+class InvalidTailError(Exception):
+    """The tail must either be two-tailed or one-tailed"""
